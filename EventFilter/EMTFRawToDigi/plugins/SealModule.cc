0b62bdf3e698ed52bee04d2e4d24d6d957c8c6a6
Pierluigi Bortignon <bortigno@gmail.com>
2015-11-18 01:32:08 +0100

    First compiling and running version of the EMTF Unpacker.

---
+++ EventFilter/EMTFRawToDigi/plugins/SealModule.cc
@@ -0,0 +1,9 @@
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include <EventFilter/EMTFRawToDigi/interface/EMTFUnpacker.h>
+// #include <EventFilter/EMTFRawToDigi/interface/EMTFPacker.h>
+// #include <EventFilter/EMTFRawToDigi/interface/EMTFAnalyzer.h>
+
+DEFINE_FWK_MODULE(EMTFUnpacker);
+// DEFINE_FWK_MODULE(EMTFPacker);
+// DEFINE_FWK_MODULE(EMTFAnalyzer);
+
