aaabe22b37d3bda090518b2a95c2e0f9d337763c
Benjamin <bmichlin@rice.edu>
2015-11-20 00:05:39 +0100

    Integration of standalone unpacker in to EMTFUnpacker package

--- EventFilter/EMTFRawToDigi/plugins/SealModule.cc
+++
@@ -1,9 +0,0 @@
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include <EventFilter/EMTFRawToDigi/interface/EMTFUnpacker.h>
-// #include <EventFilter/EMTFRawToDigi/interface/EMTFPacker.h>
-// #include <EventFilter/EMTFRawToDigi/interface/EMTFAnalyzer.h>
-
-DEFINE_FWK_MODULE(EMTFUnpacker);
-// DEFINE_FWK_MODULE(EMTFPacker);
-// DEFINE_FWK_MODULE(EMTFAnalyzer);
-
