82fbe36c7115c46036b38fb7ddd9b18303e1594d
bmichlin <bmichlin@rice.edu>
2015-12-04 18:13:28 +0100

    Andrew's updates to print hex values of words as they are unpacked.

--- EventFilter/EMTFRawToDigi/src/block_operator.cpp
+++ EventFilter/EMTFRawToDigi/src/block_operator.cpp
@@ -1,10 +1,12 @@
 #include <stdlib.h>
 #include <iostream>
+#include <iomanip>
 #include "EventFilter/EMTFRawToDigi/include/mtf7/block_operator.h"
 #include "EventFilter/EMTFRawToDigi/include/mtf7/emutf_debug.h"
 
+
 //----------------------------------------------------------------------
-mtf7::block_operator::block_operator( mtf7::error_value *error_status ): 
+mtf7::block_operator::block_operator( mtf7::error_value *error_status ):
   _buffer_start_ptr(0), _i_own_buffer(false), _error_status(error_status)
 {}
 
@@ -17,11 +19,11 @@ void mtf7::block_operator::free_own_buffer()
 {  
   if (!_i_own_buffer    ) return;
   if (!_buffer_start_ptr) return;
-
+  
   void *ptr = (void *) _buffer_start_ptr;
-
+  
   free( ptr );
-
+  
   _buffer_start_ptr = 0;
   _i_own_buffer = false;
 }
@@ -34,6 +36,16 @@ mtf7::error_value mtf7::block_operator::get_error_status()
 void mtf7::block_operator::break_into_abcd_words( word_64bit input_value ){
 
   MTF7_DEBUG( std::cout, input_value);
+
+  // AWB 04.12.15
+  std::cout << std::hex // Numbers in hexidecimal 
+            << std::showbase // Show the 0x prefix
+            << std::internal // Fill between the prefix and the number
+            << std::setfill('0'); // Fill with 0s
+
+  std::cout << "Inside break_into_abcd_words in src/block_operator.cpp" << std::endl;
+  std::cout << "Word abcd: " << std::setw(18) << input_value << std::endl;
+
 /*
   _16bit_word_a = input_value & 0xffff; input_value >>= 16;
   _16bit_word_b = input_value & 0xffff; input_value >>= 16;
@@ -44,7 +56,17 @@ void mtf7::block_operator::break_into_abcd_words( word_64bit input_value ){
   _16bit_word_d = input_value & 0xffff; input_value >>= 16;
   _16bit_word_c = input_value & 0xffff; input_value >>= 16;
   _16bit_word_b = input_value & 0xffff; input_value >>= 16;
-  _16bit_word_a = input_value & 0xffff; 
+  _16bit_word_a = input_value & 0xffff;
+  
+    // AWB 04.12.15
+  std::cout << "Word a: " << std::setw(6) << _16bit_word_a << std::endl;
+  std::cout << "Word b: " << std::setw(6) << _16bit_word_b << std::endl;
+  std::cout << "Word c: " << std::setw(6) << _16bit_word_c << std::endl;
+  std::cout << "Word d: " << std::setw(6) << _16bit_word_d << std::endl;
+
+  std::cout << std::dec // Numbers in hexidecimal 
+            << std::noshowbase; // Show the 0x prefix
+
 }
 
 //----------------------------------------------------------------------
@@ -58,7 +80,7 @@ mtf7::word_64bit mtf7::block_operator::merge_abcd_words(){
   merged_word |= _16bit_word_a & 0xffff;
 
   MTF7_DEBUG( std::cout, merged_word);
-  
+
   return merged_word;
 
 }
@@ -77,3 +99,4 @@ mtf7::word_64bit *mtf7::block_operator::create_buffer( unsigned long buffer_size
   return buffer;
 
 }
+     
