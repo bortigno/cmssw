a89e85ee2eceeb9ecc72fe02b7b61af9787c9e9d
Andrew Brinkerhoff <abrinke1@cern.ch>
2015-12-10 13:59:34 +0100

    Rolling back change to emutf_debug

--- EventFilter/EMTFRawToDigi/include/mtf7/emutf_debug.h
+++ EventFilter/EMTFRawToDigi/include/mtf7/emutf_debug.h
@@ -1,19 +1,12 @@
-/* AWB 20.11.15: Always output debug messages */
+#define DEBUG_BUILD 1
+#ifdef DEBUG_BUILD
 	#define MTF7_DEBUG( os, var ) \
   		(os) << "DBG: " << __FILE__ << "(" << __LINE__ << ") "\
     	<< #var << " = [" << (var) << "]" << std::endl
     #define MTF7_DEBUG_MSG(os, msg) \
     	(os) << "DBG: " << __FILE__ << "(" << __LINE__ << ") "\
     	<< msg << std::endl
-
-/* #ifdef DEBUG_BUILD */
-/* 	#define MTF7_DEBUG( os, var ) \ */
-/*   		(os) << "DBG: " << __FILE__ << "(" << __LINE__ << ") "\ */
-/*     	<< #var << " = [" << (var) << "]" << std::endl */
-/*     #define MTF7_DEBUG_MSG(os, msg) \ */
-/*     	(os) << "DBG: " << __FILE__ << "(" << __LINE__ << ") "\ */
-/*     	<< msg << std::endl */
-/* #else */
-/*     #define MTF7_DEBUG(os, var) do {} while (0) */
-/*     #define MTF7_DEBUG_MSG(os, msg) do {} while (0) */
-/* #endif */
+#else
+    #define MTF7_DEBUG(os, var) do {} while (0)
+    #define MTF7_DEBUG_MSG(os, msg) do {} while (0)
+#endif
